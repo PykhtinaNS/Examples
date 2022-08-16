174223c5e80fcdce8a6cef450f6e478b6df30905
Natalya Pykhtina <sovanst4@yandex.ru>
2022-08-16 14:23:45 +0300

    Ex005

---
+++ Ex005_If/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+if(username.ToLower()=="наташа")
+{
+  Console.WriteLine("Ура! Это же Наташа!");  
+}
+else
+{
+  Console.Write("Привет, ");
+  Console.WriteLine(username);
+}
